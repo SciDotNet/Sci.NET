5d77ac609e4fe3fbb5c36ab2dc54335d90c5684e
Reece Noone <reece.noone@outlook.com>
2024-01-24 22:56:26 +0000

    Added SIMD for Matrix Multiply

---
+++ src/Sci.NET.Mathematics.Benchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Sci.NET Foundation. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+
+using BenchmarkDotNet.Running;
+using Sci.NET.Mathematics.Benchmarks.SIMD;
+
+BenchmarkRunner.Run<MatrixMultiplyBenchmarks>();
--- src/Sci.NET.Mathematics/Backends/Managed/ManagedLinearAlgebraKernels.cs
+++ src/Sci.NET.Mathematics/Backends/Managed/ManagedLinearAlgebraKernels.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
 using System.Numerics;
+using System.Runtime.CompilerServices;
 using Sci.NET.Common.Concurrency;
 using Sci.NET.Common.Memory;
 using Sci.NET.Mathematics.Tensors;
@@ -10,9 +11,21 @@ namespace Sci.NET.Mathematics.Backends.Managed;
 
 internal class ManagedLinearAlgebraKernels : ILinearAlgebraKernels
 {
+    [MethodImpl(MethodImplOptions.NoOptimization)]
     public void MatrixMultiply<TNumber>(Matrix<TNumber> left, Matrix<TNumber> right, Matrix<TNumber> result)
         where TNumber : unmanaged, INumber<TNumber>
     {
+        // BUG: Vector<TNumber> has strange behaviour when sizeof(TNumber) > 8
+        if (System.Numerics.Vector<TNumber>.IsSupported && Vector.IsHardwareAccelerated && Unsafe.SizeOf<TNumber>() < 8)
+        {
+            MatrixMultiplySimd(
+                left,
+                right,
+                result);
+
+            return;
+        }
+
         var leftMemoryBlock = (SystemMemoryBlock<TNumber>)left.Memory;
         var rightMemoryBlock = (SystemMemoryBlock<TNumber>)right.Memory;
         var resultMemoryBlock = (SystemMemoryBlock<TNumber>)result.Memory;
@@ -49,5 +62,48 @@ internal class ManagedLinearAlgebraKernels : ILinearAlgebraKernels
             left.Length,
             ManagedTensorBackend.ParallelizationThreshold / 2,
             i => resultMemoryBlock[0] += leftMemoryBlock[i] * rightMemoryBlock[i]);
+    }
+
+    private static void MatrixMultiplySimd<TNumber>(
+        Matrix<TNumber> left,
+        Matrix<TNumber> right,
+        Matrix<TNumber> result)
+        where TNumber : unmanaged, INumber<TNumber>
+    {
+        var leftMemoryBlock = (SystemMemoryBlock<TNumber>)left.Memory;
+        var rightMemoryBlock = (SystemMemoryBlock<TNumber>)right.Memory;
+        var resultMemoryBlock = (SystemMemoryBlock<TNumber>)result.Memory;
+        var vectorSize = System.Numerics.Vector<TNumber>.Count;
+        var kLength = left.Columns;
+        var jLength = result.Columns;
+
+        LazyParallelExecutor.For(
+            0,
+            result.Rows,
+            0,
+            result.Columns,
+            ManagedTensorBackend.ParallelizationThreshold / 2,
+            (i, j) =>
+            {
+                var sum = TNumber.Zero;
+                var k = 0;
+
+                for (; k <= kLength - vectorSize; k += vectorSize)
+                {
+                    var leftSpan = leftMemoryBlock.AsSpan((i * kLength) + k, vectorSize);
+                    var rightSpan = rightMemoryBlock.AsSpan((k * jLength) + j, vectorSize);
+                    var vLeft = new System.Numerics.Vector<TNumber>(leftSpan);
+                    var vRight = new System.Numerics.Vector<TNumber>(rightSpan);
+
+                    sum += Vector.Dot(vLeft, vRight);
+                }
+
+                for (; k < kLength; k++)
+                {
+                    sum += leftMemoryBlock[(i * kLength) + k] * rightMemoryBlock[(k * jLength) + j];
+                }
+
+                resultMemoryBlock[(i * jLength) + j] = sum;
+            });
     }
 }
