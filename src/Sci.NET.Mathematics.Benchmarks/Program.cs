e4f71a1a443b82d51922ff998395e57c79e41743
Reece Noone <reece.noone@outlook.com>
2024-02-21 16:57:23 +0000

    Added coverage exclusion for benchmarks

--- src/Sci.NET.Mathematics.Benchmarks/Program.cs
+++ src/Sci.NET.Mathematics.Benchmarks/Program.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) Sci.NET Foundation. All rights reserved.
 // Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
 using BenchmarkDotNet.Running;
 using Sci.NET.Mathematics.Benchmarks.SIMD;
+
+[assembly:ExcludeFromCodeCoverage]
 
 BenchmarkRunner.Run<AdditionBenchmarks>();
 
