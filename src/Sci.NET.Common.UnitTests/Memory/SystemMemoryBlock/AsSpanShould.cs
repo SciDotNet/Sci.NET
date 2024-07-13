d74f4abefe7c82b5cd0f9bc0b97848f9804ac66a
Reece Noone <reece.noone@outlook.com>
2024-07-13 13:13:31 +0100

    Added SystemMemoryBlock tests

--- src/Sci.NET.Common.UnitTests/Memory/SystemMemoryBlock/AsSpanShould.cs
+++ src/Sci.NET.Common.UnitTests/Memory/SystemMemoryBlock/AsSpanShould.cs
@@ -25,6 +25,23 @@ public class AsSpanShould
     }
 
     [Fact]
+    public void ReturnCorrectSpan_GivenValidInstanceAndSubRange()
+    {
+        var data = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        using var memoryBlock = new SystemMemoryBlock<int>(10);
+        memoryBlock.CopyFrom(data);
+
+        var span = memoryBlock.AsSpan(2, 5);
+
+        span.Length.Should().Be(5);
+
+        for (var i = 0; i < span.Length; i++)
+        {
+            span[i].Should().Be(data[i + 2]);
+        }
+    }
+
+    [Fact]
     public void ThrowException_WhenDisposed()
     {
         var memoryBlock = new SystemMemoryBlock<int>(10);
@@ -33,5 +50,15 @@ public class AsSpanShould
         Action act = () => memoryBlock.AsSpan();
 
         act.Should().Throw<ObjectDisposedException>();
+    }
+
+    [Fact]
+    public void ThrowException_WhenUsingLongRange()
+    {
+        var memoryBlock = new SystemMemoryBlock<int>((long)int.MaxValue + 10);
+
+        Action act = () => memoryBlock.AsSpan();
+
+        act.Should().Throw<InvalidOperationException>();
     }
 }
---
+++ src/Sci.NET.Common.UnitTests/Memory/SystemMemoryBlock/UnsafeFreeMemoryShould.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Sci.NET Foundation. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+
+using Sci.NET.Common.Memory;
+
+namespace Sci.NET.Common.UnitTests.Memory.SystemMemoryBlock;
+
+public class UnsafeFreeMemoryShould
+{
+    [Fact]
+    public void FreeMemory()
+    {
+        // Arrange
+        using var block = new SystemMemoryBlock<int>(128);
+
+        // Act
+        block.UnsafeFreeMemory();
+
+        // Assert
+        block.IsDisposed.Should().BeTrue();
+    }
+}
