2ab9e369a43c0e776ab9bdacb686847997d099ad
Reece Noone <reece.noone@outlook.com>
2024-07-16 21:22:22 +0100

    Added Reduction and Arithmetic examples

--- Samples/Basics.MatrixMultiply/Program.cs
+++ Samples/Basics.MatrixMultiply/Program.cs
@@ -1,17 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Sci.NET.Mathematics.Tensors;
 
-using Sci.NET.Mathematics.Tensors;
-
+// Multiply two matrices with shapes [2, 2] and [2, 2].
 using var matrixA = Tensor.FromArray<int>(new int[,] { { 1, 2 }, { 3, 4 } }).ToMatrix();
 using var matrixB = Tensor.FromArray<int>(new int[,] { { 5, 6 }, { 7, 8 } }).ToMatrix();
 
+// Multiply two matrices.
 var result = matrixA.MatrixMultiply(matrixB);
 
-Console.WriteLine(result); // Output: [[19, 22], [43, 50]]
+// Output: [[19, 22], [43, 50]]
+Console.WriteLine(result);
 
-using var randomMatrixA = Tensor.Random.Uniform(new Shape(100,500), 0.0f, 1.0f).ToMatrix();
-using var randomMatrixB = Tensor.Random.Uniform(new Shape(500,200), 0.0f, 1.0f).ToMatrix();
+// Multiply two random matrices with shapes [100, 500] and [500, 200].
+using var randomMatrixA = Tensor.Random.Uniform(new Shape(100, 500), 0.0f, 1.0f).ToMatrix();
+using var randomMatrixB = Tensor.Random.Uniform(new Shape(500, 200), 0.0f, 1.0f).ToMatrix();
 
+// Multiply two the two matrices.
 var randomResult = randomMatrixA.MatrixMultiply(randomMatrixB);
 
-Console.WriteLine(randomResult); // Output: Matrix with shape [100, 200]
+// Output: Matrix with shape [100, 200]
+Console.WriteLine(randomResult);
---
+++ Samples/Basics.Reduction/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using Sci.NET.Mathematics.Tensors;
+
+var tensor = Tensor.FromArray<int>(new int[,] { { 1, 2 }, { 3, 4 } });
+
+var result1 = tensor.Sum([0]);
+var result2 = tensor.Sum([1]);
+
+Console.WriteLine(result1); // Output: [4, 6]
+Console.WriteLine(result2); // Output: [3, 7]
