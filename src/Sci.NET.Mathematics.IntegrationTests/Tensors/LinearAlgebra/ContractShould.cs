e1995e8e53b7e7b75f53c5a5cfb72658a5e30f92
Reece Noone <reece.noone@outlook.com>
2024-08-26 00:15:40 +0100

    Fixed issue with 2D partitioner

--- src/Sci.NET.Mathematics.IntegrationTests/Tensors/LinearAlgebra/ContractShould.cs
+++ src/Sci.NET.Mathematics.IntegrationTests/Tensors/LinearAlgebra/ContractShould.cs
@@ -92,7 +92,5 @@ public class ContractShould : IntegrationTestBase
 
         // Assert
         result.Should().HaveEquivalentElements(expected);
-
-        // This actually found {{1720, 1900}, {1782, 1972}, {1844, 2044}, {1906, 2116}}
     }
 }
