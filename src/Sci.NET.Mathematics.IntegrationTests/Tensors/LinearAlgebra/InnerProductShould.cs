206990a4285f6dcac23f4bfb9ffc8293f92ea588
Reece Noone <reece.noone@outlook.com>
2025-06-02 00:13:55 +0100

    Added inner product test

---
+++ src/Sci.NET.Mathematics.IntegrationTests/Tensors/LinearAlgebra/InnerProductShould.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Sci.NET Foundation. All rights reserved.
+// Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
+
+using Sci.NET.Mathematics.Backends.Devices;
+using Sci.NET.Mathematics.Tensors;
+using Sci.NET.Tests.Framework.Assertions;
+using Sci.NET.Tests.Framework.Integration;
+
+namespace Sci.NET.Mathematics.IntegrationTests.Tensors.LinearAlgebra;
+
+public class InnerProductShould : IntegrationTestBase
+{
+    [Theory]
+    [MemberData(nameof(ComputeDevices))]
+    public void ComputeInnerProduct(IDevice device)
+    {
+        using var left = Tensor.FromArray<float>(new float[] { 1, 2, 3, 4, 5, 6, 7, 8 }).WithGradient().ToVector();
+        using var right = Tensor.FromArray<float>(new float[] { 8, 7, 6, 5, 4, 3, 2, 1 }).WithGradient().ToVector();
+
+        left.To(device);
+        right.To(device);
+
+        using var result = left.Inner(right);
+
+        result.Backward();
+
+        result.Value.Should().Be(120);
+
+        left.Gradient!.Should().NotBeNull();
+        left.Gradient!.Should().HaveApproximatelyEquivalentElements(new float[] { 8, 7, 6, 5, 4, 3, 2, 1 }, 1e-6f);
+
+        right.Gradient!.Should().NotBeNull();
+        right.Gradient!.Should().HaveApproximatelyEquivalentElements(new float[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 1e-6f);
+    }
+}
